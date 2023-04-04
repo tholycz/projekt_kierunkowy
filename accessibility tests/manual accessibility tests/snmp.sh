d4c314585dd6764c34a7d29f8286da74a9e22976
Tatiana Hołycz <91832933+tholycz@users.noreply.github.com>
2023-04-04 13:45:34 +0200

    upload snmp.sh

---
+++ accessibility tests/manual accessibility tests/snmp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cd /home/tatiana/skrypty/snmp
+clear
+echo "**********************************************"
+echo "*                                            *"
+echo "* Konsola testów manualnych sprawności sieci *"
+echo "* autorstwa Tatiany Hołycz                   *"
+echo "*                                            *"
+echo "**********************************************"
+echo " "
+PS3="Wybierz urządzenie, które chcesz sprawdzić za pomocą protokołu snmp:"
+select device in "serwer nas" "serwer wirtualizacji A" "koniec"
+do
+  case $device in
+    "serwer nas")
+      bash snmp_nas.sh ;;
+    "serwer wirtualizacji A")
+      bash snmp_serwerA.sh ;;
+    "koniec")
+      echo "Testy manualne zostały zakończone"
+      break ;;
+    *)
+      echo "wybrałeś nieprawidłową opcję" ;;
+    esac
+done
